7bca5cc42f74878db32dc4b1aae74d0ea8d46484
Florian Schweizer <post.an.flo@gmail.com>
2021-08-17 20:22:28 +0200

    Update Buttonstyle, Reorganise structure

---
+++ Sources/CommonStyles/ButtonStyles/WideButtonStyle.swift
@@ -0,0 +1,38 @@
+//
+//  WideButtonStyle.swift
+//  
+//
+//  Created by florian schweizer on 15.03.21.
+//
+
+import SwiftUI
+
+public struct WideButtonStyle: ButtonStyle {
+    private let isDestructive: Bool
+    private let color: Color
+    private let cornerRadius: CGFloat
+    
+    public init(isDestructive: Bool = false, color: Color = .blue, cornerRadius: CGFloat = 10) {
+        self.isDestructive = isDestructive
+        self.color = color
+        self.cornerRadius = cornerRadius
+    }
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Spacer()
+            
+            configuration.label
+                .foregroundColor(.primary)
+                .font(Font.body.weight(.semibold))
+            
+            Spacer()
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(isDestructive ? Color.red : color)
+                .opacity(configuration.isPressed ? 0.75 : 1)
+        )
+    }
+}
