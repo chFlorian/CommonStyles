7152eb026a62b18f6a857f7d2b73ba1f64f76f13
Florian Schweizer <post.an.flo@gmail.com>
2021-08-27 10:51:40 +0200

    Add symbol based inits

---
+++ Sources/CommonStyles/HelperInits/SymbolBasedInits.swift
@@ -0,0 +1,44 @@
+//
+//  SymbolBasedInits.swift
+//  
+//
+//  Created by florian schweizer on 27.08.21.
+//
+
+import SwiftUI
+
+extension Link where Label == HStack<TupleView<(Image, Text, Spacer)>> {
+    public init(_ title: String, symbol: String, destination: URL) {
+        self.init(destination: destination, label: {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            } as! Label
+        })
+    }
+}
+
+extension NavigationLink where Label == HStack<TupleView<(Image, Text, Spacer)>> {
+    public init(_ title: String, symbol: String, destination: Destination) {
+        self.init(destination: destination) {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            } as! Label
+        }
+    }
+}
+
+extension Button where Label == HStack<TupleView<(Image, Text, Spacer)>> {
+    public init(_ title: String, symbol: String, action: @escaping () -> Void) {
+        self.init(action: action) {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            } as! Label
+        }
+    }
+}
