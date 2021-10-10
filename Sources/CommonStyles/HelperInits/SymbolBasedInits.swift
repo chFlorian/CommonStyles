bb1935f254d6479b3e5e176479e463901ab42e25
Florian Schweizer <post.an.flo@gmail.com>
2021-10-10 19:33:48 +0200

    Add LocalizedStringKey variants for helper inits

--- Sources/CommonStyles/HelperInits/SymbolBasedInits.swift
+++ Sources/CommonStyles/HelperInits/SymbolBasedInits.swift
@@ -17,6 +17,16 @@ extension Link where Label == HStack<TupleView<(Image, Text, Spacer)>> {
             }
         })
     }
+    
+    public init(_ title: LocalizedStringKey, symbol: String, destination: URL) {
+        self.init(destination: destination, label: {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            }
+        })
+    }
 }
 
 extension NavigationLink where Label == HStack<TupleView<(Image, Text, Spacer)>> {
@@ -29,10 +39,30 @@ extension NavigationLink where Label == HStack<TupleView<(Image, Text, Spacer)>>
             }
         }
     }
+    
+    public init(_ title: LocalizedStringKey, symbol: String, destination: Destination) {
+        self.init(destination: destination) {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            }
+        }
+    }
 }
 
 extension Button where Label == HStack<TupleView<(Image, Text, Spacer)>> {
     public init(_ title: String, symbol: String, action: @escaping () -> Void) {
+        self.init(action: action) {
+            HStack(spacing: 12) {
+                Image(systemName: symbol)
+                Text(title)
+                Spacer()
+            }
+        }
+    }
+    
+    public init(_ title: LocalizedStringKey, symbol: String, action: @escaping () -> Void) {
         self.init(action: action) {
             HStack(spacing: 12) {
                 Image(systemName: symbol)
